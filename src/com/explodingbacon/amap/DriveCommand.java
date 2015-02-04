59169df360e163d73779f2e423ff62496d7b660a
Ryan Shavell <superweegee1@comcast.net>
2015-02-03 20:59:09 -0500

    Re-did angle calculation again and wrote code for detecting if we should turn left or right. The angles are now perfectly accurate at all times.

--- src/com/explodingbacon/amap/DriveCommand.java
+++ src/com/explodingbacon/amap/DriveCommand.java
@@ -23,6 +23,7 @@ public class DriveCommand extends Command {
         } else {
             line1 = new Line2D.Double(0, Board.robot.y + Board.robot.height, 0, Board.robot.y);
         }           
+        System.out.println("==========================");
         double angle = angle(line1, line2);
         angle *= 57.2957795; //Convert from radians to degrees
         System.out.println("Turn Angle: " + angle + " degrees.");
@@ -32,13 +33,40 @@ public class DriveCommand extends Command {
         double inchDistance = Math.sqrt((endY - startY) * (endY - startY) + (endX - startX) * (endX - startX));
         inchDistance = Main.scaleDown(inchDistance);
         double distance = inchDistance / (Math.PI * 4) * 1024; //Convert from inches to encoder clicks
-        System.out.println("Drive Distance : " + inchDistance + " inches.");
+        //System.out.println("Drive Distance : " + inchDistance + " inches.");
         data.add(new String[]{"drive", distance + "", distance + ""});       
     }
     
-    public static double angle(Line2D line1, Line2D line2) {
-        double angle1 = Math.atan2(line1.getY1() - line1.getY2(), line1.getX1() - line1.getX2());
-        double angle2 = Math.atan2(line2.getY1() - line2.getY2(), line2.getX1() - line2.getX2());
-        return Math.abs(angle1) - Math.abs(angle2);
+    public double angle(Line2D line1, Line2D line2) {
+        //double angle1 = Math.atan2(line1.getY1() - line1.getY2(), line1.getX1() - line1.getX2());
+        //double angle2 = Math.atan2(line2.getY1() - line2.getY2(), line2.getX1() - line2.getX2());
+        //System.out.println("Angle1: " + angle1 + ", Angle2: " + angle2);
+        //double angle = Math.abs(angle1) - Math.abs(angle2);
+        double xDiff1 = line1.getX2() - line1.getX1();
+        double xDiff2 = line2.getX2() - line2.getX1();
+        double yDiff1 = line1.getY2() - line1.getY1();
+        double yDiff2 = line2.getY2() - line2.getY1();
+        double angle = Math.atan2(xDiff1 * yDiff2 - yDiff1 * xDiff2, xDiff1 * xDiff2 + yDiff1 * yDiff2);
+        double[] crossProduct = crossProduct(new double[]{xDiff1, yDiff1, 0}, new double[]{xDiff2, yDiff2, 0});
+        if (Main.sign(crossProduct[2]) == -1) { //Turn left
+            if (angle < 0) {
+                angle *= -1;
+            }
+        } else { //Turn right
+            if (angle > 0) {
+                angle *= -1;
+            }
+        }
+        return angle;
+    }
+    
+    public double[] crossProduct(double[] v0, double[] v1) {
+        double crossProduct[] = new double[3];
+
+        crossProduct[0] = v0[1] * v1[2] - v0[2] * v1[1];
+        crossProduct[1] = v0[2] * v1[0] - v0[0] * v1[2];
+        crossProduct[2] = v0[0] * v1[1] - v0[1] * v1[0];
+
+        return crossProduct;
     }
 }
